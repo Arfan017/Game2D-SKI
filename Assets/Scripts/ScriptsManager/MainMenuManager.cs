a4634bd1404669a3c12bce9ef5ca4541dc73a0bd
Arfan017 <59225236+Arfan017@users.noreply.github.com>
2024-05-28 07:33:26 -0700

    add folder Scripts

---
+++ Assets/Scripts/ScriptsManager/MainMenuManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        
+    }
+}
